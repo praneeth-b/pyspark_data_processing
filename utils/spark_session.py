b2a9763c8dda81283035ef3c967ef3e414590566
praneeth-b <balakrishna.praneeth@gmail.com>
2025-11-24 08:44:25 +0100

    spark session creator using modified singleton pattern.

---
+++ utils/spark_session.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+import logging
+
+
+class SparkSessionManager:
+    _instance = None
+
+    @classmethod
+    def get_spark_session(cls, app_name="YelpDataPipeline", memory="4g"):
+        if cls._instance is None:
+            cls._instance = (
+                SparkSession.builder
+                .appName(app_name)
+                .master("local[*]")  # running on my local pc
+                .config("spark.driver.memory", memory)
+                .config("spark.sql.adaptive.enabled", "true")
+                .config("spark.sql.adaptive.coalescePartitions.enabled", "true")
+                .config("spark.sql.shuffle.partitions", "8")
+                .getOrCreate()
+            )
+            cls._instance.sparkContext.setLogLevel("WARN")
+            logging.info(f"Spark session created: {app_name}")
+        return cls._instance
+
+    @classmethod
+    def stop(cls):
+        if cls._instance:
+            cls._instance.stop()
+            cls._instance = None
